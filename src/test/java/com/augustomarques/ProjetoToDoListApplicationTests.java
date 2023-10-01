cf3294177b87380d43ef87ee5d381cd06e675f00
Augusto Marques <markesaf16@gmail.com>
2023-10-01 00:12:08 -0300

    Implementando Swagger e Conexao com Banco De Dados MySQL

---
+++ src/test/java/com/augustomarques/ProjetoToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augustomarques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
